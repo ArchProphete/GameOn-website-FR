99d45e50e8c8fd00b956ab01fe3f167ddae3e981
Ayoub Assairh <ayoub.assairh@vif.fr>
2021-08-24 16:14:56 +0200

    Add new User class. Fix few things in html

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -11,6 +11,45 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
+const form = document.querySelector("form");
+
+class Validator {
+  constructor() {
+  }
+
+  get ValidLastString() {
+    return this.validLastString()
+  }
+
+  validLastString (string) {
+    return string.length <= 2
+  }
+}
+
+class User {
+  constructor(
+      firstName,
+      lastName,
+      email,
+      birthDate,
+      howMuchTournament,
+      tournamentTown,
+      cgvAllowed,
+      newsletterAllowed,
+  ) {
+    this.firstName = document.getElementById('first').value;
+    this.lastName = document.getElementById('last').value;
+    this.email = document.getElementById('email').value;
+    this.birthDate = document.getElementById('birthdate').value;
+    this.howMuchTournament = document.getElementById('quantity').value;
+    this.tournamentTown = document.querySelector('input[name="location"]:checked').value;
+    this.cgvAllowed = document.querySelector('#checkbox1:checked').value;
+    this.newsletterAllowed = document.querySelector('#checkbox2:checked')?.value;
+  }
+}
+
+// this.tournamentTown = document.querySelector('input[name="location"]:checked').value;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +59,16 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal when you click on the Close span
+closeModal.addEventListener("click",(e) => {
+  modalbg.style.display = "none";
+});
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+})
+
+function validate() {
+  const newUser = new User()
+  console.log(newUser.tournamentTown);
+}
